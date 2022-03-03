a1504115d84b8dd136048f494c524445ac70c208
suigang <244153653@qq.com>
2022-03-03 22:04:49 +0800

    feat(修改): 修改

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-
+/* eslint-disable */
 import Ref from './components/Ref.vue';
 import Reactive from './components/Reactive.vue';
 import PropsAndEmits from './components/PropsAndEmits.vue';
