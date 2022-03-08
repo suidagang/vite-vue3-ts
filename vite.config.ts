9a8710f4b07c3bc7f456319e28948830759f2242
suigang <244153653@qq.com>
2022-03-08 16:59:59 +0800

    feat: 系统新增环境变量

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,34 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv, UserConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    host: '127.0.0.1', //解决"vite use `--host` to expose"
-    port: 3001,
-    open: true,
-    proxy: {
-      //这里是通过请求/api 来转发到 https://api.pingping6.com/
-      //假如你要请求https://api.*.com/a/b
-      //那么axios的url，可以配置为 /api/a/b
-      '^/api': {
-        target: 'http://localhost:3000/',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-        // configure: (proxy, options) => {
-        //   // proxy 是 'http-proxy' 的实例
-        // }
+export default defineConfig(({ mode }: UserConfig): UserConfig => {
+  const env = loadEnv(mode, __dirname);
+  return {
+    server: {
+      host: '127.0.0.1', //解决"vite use `--host` to expose"
+      port: Number(env.VITE_PORT),
+      open: true,
+      proxy: {
+        '^/api': {
+          target: 'http://localhost:3000/',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
       }
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src')
+      }
+    },
+    plugins: [vue()],
+    build: {
+      // 是否开启sourcemap
+      sourcemap: false,
+      brotliSize: false,
+      // 消除打包大小超过500kb警告
+      chunkSizeWarningLimit: 2000
     }
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  },
-  plugins: [vue()],
-  build: {
-    // 是否开启sourcemap
-    sourcemap: false,
-    brotliSize: false,
-    // 消除打包大小超过500kb警告
-    chunkSizeWarningLimit: 2000
-  }
+  };
 });
