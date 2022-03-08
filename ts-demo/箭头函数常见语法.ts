e61b16ed43b6e9b79eedb2a8c1430dadfd7a1b5d
suigang <244153653@qq.com>
2022-03-08 14:57:27 +0800

    feat: 完善

---
+++ ts-demo/箭头函数常见语法.ts
@@ -0,0 +1,12 @@
+let myBooks = [1,2,3,4,5];
+myBooks.forEach(() => console.log('reading'));
+
+myBooks.forEach(title => console.log(title));
+
+myBooks.forEach((title, idx, arr) =>
+  console.log(idx + '-' + title)
+);
+
+myBooks.forEach((title, idx, arr) => {
+  console.log(idx + '-' + title);
+});
