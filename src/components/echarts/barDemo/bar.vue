ee16e1409d73e3b00c20c89027cef4c58f0b4a09
suigang <244153653@qq.com>
2022-03-23 17:30:50 +0800

    feat: 文档设计

---
+++ src/components/echarts/barDemo/bar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="demo-box">
+    <div class="echarts-box">
+      <ReBar />
+    </div>
+    <div class="set-box">
+      <div class="set-title">公用设置</div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import ReBar from '../bar/ReBar.vue';
+</script>
+
+<style lang="less" scoped>
+.demo-box {
+  width: 100%;
+  height: 250px;
+  display: flex;
+}
+.echarts-box {
+  flex: 1;
+  height: 250px;
+  background: #01145e;
+}
+.set-box {
+  flex: 1;
+  height: 250px;
+  background: pink;
+}
+.set-title {
+  height: 40px;
+  line-height: 40px;
+  color: #000;
+  font-weight: bold;
+  font-size: 16px;
+  padding-left: 20px;
+  box-sizing: border-box;
+}
+</style>
